70c3ff29b57f544ba0ee2c3d6a57fadc6a31e5ed
yongarframe <yongar2002@gmail.com>
2025-04-25 15:00:37 +0900

    🌱 포켓못 데이터 통신 API 연결
    포켓못 API 데이터 연결

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMultiplePokemonById } from "./RTK/thunk";
+
+function App() {
+  const dispatch = useDispatch();
+  const pokemonData = useSelector((state) => state.pokemon);
+  console.log(pokemonData);
+  useEffect(() => {
+    dispatch(fetchMultiplePokemonById(151));
+  }, []);
+
+  return <></>;
+}
+
+export default App;
---
+++ src/RTK/slice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchMultiplePokemonById } from "./thunk";
+
+export const pokemonSlice = createSlice({
+  name: "pokemon",
+  initialState: {
+    data: [],
+    loading: true,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMultiplePokemonById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchMultiplePokemonById.rejected, (state) => {
+        state.loading = false;
+      })
+      .addCase(fetchMultiplePokemonById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      });
+  },
+});
